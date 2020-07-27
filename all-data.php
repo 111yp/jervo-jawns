48be73e5fcdf392bf792699889b56575c159690c
Hunter Hampton <hmh84@drexel.edu>
2020-07-27 14:57:44 -0400

    :back: Backend UI Instances
    -Created sql+php instances of all datapoints found within the UI w/ basic CSS
    -Added all driver profile pics
    -Added icon svg's for datapoints (They are optimized)

--- all-data.php
+++ all-data.php
@@ -57,7 +57,7 @@
                 } else {
                     echo "There is no data to display..";
                 }
-                ?>
+    ?>
 
                 </tbody>
             </table>
