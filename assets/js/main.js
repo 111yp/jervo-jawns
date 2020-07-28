2c48c842452911c59066b9868509036ea4de04a4
Hunter Hampton <hmh84@drexel.edu>
2020-07-28 18:56:35 -0400

    :wrench: Fixed Nav
    -Fixed html and css for nav
    -Added main section in html
    -Added responsive JS to apply top padding to main equal to whatever the height of the nav is

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("#menu").slideToggle("slow");
-        //OLD HAMBURGER MENU
-        /*
-        if ($("#menu-img").attr('src') == 'assets/svg/hamburger.svg')
-        {
-            $("#menu-img").attr('src', 'assets/svg/cancel.svg');
-        }
-        else
-        {
-            $("#menu-img").attr('src', 'assets/svg/hamburger.svg');
-        }
-        */
-    });
-  });
-  
+// Set top padding to main based on nav height
+
+const nav = document.querySelector('nav');
+const main = document.querySelector('main');
+
+window.onresize = responsivenessCheck;
+window.onload = initCheck;
+
+function initCheck() {
+    responsivenessCheck();
+    window.scrollTo(0, 0);
+}
+
+function responsivenessCheck() {
+    var x = nav.clientHeight;
+    main.style.paddingTop = x+'px';
+}
