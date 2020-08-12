e1314bc5d531b4c00dd6a80f3fee6d3f6802933a
111yp <42048931+111yp@users.noreply.github.com>
2020-08-12 16:52:44 -0400

    add landscape-warning to prototype.html
    add landscape-warning to prototype.html

--- beta/include/head.php
+++ beta/include/head.php
@@ -8,6 +8,7 @@
     <title><?php echo $page_title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="assets\css\prototype-pages.css">
+    <link rel="stylesheet" href="assets\css\prototype-shared.css">
     <link href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>
     <script src="assets/js/main.js" defer></script>
