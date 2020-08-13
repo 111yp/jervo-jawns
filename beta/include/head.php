f6436e1131538054adb2c9c91b75f9256eeb70b3
Hunter Hampton <hmh84@drexel.edu>
2020-08-12 23:28:55 -0400

    :city_sunrise: Favicons
    * Added Favicons

--- beta/include/head.php
+++ beta/include/head.php
@@ -12,4 +12,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>
     <script src="assets/js/main.js" defer></script>
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
