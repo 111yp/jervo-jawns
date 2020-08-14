bb96332f289cf6e058dd5e001c9b495e16f66276
Hunter Hampton <hmh84@drexel.edu>
2020-08-14 00:24:49 -0400

    Styling & Profiling
    * Swapped map.png for better quality pic
    * Using .shadow class on prototype.php

--- beta/assets/js/prototype.js
+++ beta/assets/js/prototype.js
@@ -3,11 +3,11 @@ function openNav() {
     $(".sidenav").css({
         "box-shadow": "1px 10px 20px 7px rgba(0,0,0,0.15)"
     })
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    $(".sidenav").css({
-        "box-shadow": "none"
-    })
-  }
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  $(".sidenav").css({
+      "box-shadow": "none"
+  })
+}
